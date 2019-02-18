d998754a36e977e3fe7a8375c54ed08f87d61008
Pierguido Lambri <plambri@redhat.com>
2019-02-18 17:55:03 +0000

    Qutebrowser changes
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -3,11 +3,12 @@ c = c  # noqa: F821 pylint: disable=E0602,C0103
 config = config  # noqa: F821 pylint: disable=E0602,C0103
 
 
+# Load the autoconfig.yml configuration
+config.load_autoconfig()
+
 c.tabs.position = "left"
 c.tabs.width = "12%"
 c.completion.shrink = True
 c.editor.command = ["termite", "-e", "nvim '{}'"]
 c.content.javascript.enabled = True
 
-# Load the autoconfig.yml configuration
-config.load_autoconfig()
