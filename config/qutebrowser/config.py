5bef58c0880f104f47ffc7915a90308574eae40d
PL <pl@whats.com>
2020-10-20 14:30:39 +0100

    Removed a couple of search engines and added lazy load of tabs
    Signed-off-by: PL <pl@whats.com>

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -12,12 +12,19 @@ c.completion.shrink = True
 c.editor.command = ["termite", "-e", "nvim '{}'"]
 c.content.javascript.enabled = True
 c.qt.args = ["auth-server-whitelist=*redhat.com"]
+
+# Prevents *all* tabs from being loaded on restore, only loads on activating them
+c.session.lazy_restore = True
+
+# Search engines
 c.url.searchengines = {
     "DEFAULT": "https://duckduckgo.com/?q={}",
     "g": "https://www.google.com/search?q={}",
     "r": "https://reddit.com/r/{}",
     "w": "https://en.wikipedia.org/w/index.php?search={}",
     "gh": "https://github.com/search?q={}",
+    "yt": "https://www.youtube.com/results?search_query={}",
+    "maps": "https://www.google.fr/maps?q={}",
     "rstd": "https://doc.rust-lang.org/std/index.html?search={}",
     "crate": "https://crates.io/search?q={}",
     "rdocs": "https://docs.rs/releases/search?query={}",
