55658c3f85c92b6f0d854781f6d4da79b123f1cc
Pierguido Lambri <pierg75@yahoo.it>
2022-08-23 21:28:51 +0100

    Added missing function
    Signed-off-by: Pierguido Lambri <pierg75@yahoo.it>

--- config/nvim-0.5/lua/config/_nvimcmp.lua
+++ config/nvim-0.5/lua/config/_nvimcmp.lua
@@ -1,3 +1,9 @@
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+
 local cmp = require 'cmp'
 local lspkind = require('lspkind')
 cmp.setup({
@@ -44,8 +50,6 @@ cmp.setup({
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
       elseif has_words_before() then
         cmp.complete()
       else
