4cb0f725a407d0d05c687cfd0c3e21185ae8e0f3
Pierguido Lambri <pierg75@yahoo.it>
2022-09-05 09:04:59 +0100

    Added bashls lsp
    Signed-off-by: Pierguido Lambri <pierg75@yahoo.it>

--- config/nvim-0.5/lua/lsp/init.lua
+++ config/nvim-0.5/lua/lsp/init.lua
@@ -66,7 +66,7 @@ local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 -- clangd server will be initialised by the clangd_extension plugin
-local servers = { 'rust_analyzer', 'pyright' }
+local servers = { 'rust_analyzer', 'pyright', 'bashls' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
