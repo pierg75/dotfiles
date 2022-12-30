1759a8c3ee0755c6afd068d63a41aa430fdb024f
Pierguido Lambri <pierg75@yahoo.it>
2022-12-30 15:16:02 +0000

    Adjusted gitsigns
    Signed-off-by: Pierguido Lambri <pierg75@yahoo.it>

--- config/nvim-0.5/lua/config/gitsigns.lua
+++ config/nvim-0.5/lua/config/gitsigns.lua
@@ -1,16 +1,36 @@
-require("gitsigns").setup({
-  keymaps = {},
-  preview_config = { border = border },
-  status_formatter = function(status)
-    return " "
-      .. (status.head == "" and "detached HEAD" or status.head)
-      .. (status.added and status.added > 0 and "  " .. status.added or "")
-      .. (status.changed and status.changed > 0 and "  " .. status.changed or "")
-      .. (
-        status.removed
-          and status.removed > 0
-          and "  " .. status.removed
-        or ""
-      )
-  end,
-})
+local status_ok, gitsigns = pcall(require, "gitsigns")
+if not status_ok then
+  return
+end
+
+gitsigns.setup {
+  signs = {
+    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+  },
+  signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+  watch_gitdir = {
+    interval = 1000,
+    follow_files = true,
+  },
+  attach_to_untracked = true,
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+  },
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = "single",
+    style = "minimal",
+    relative = "cursor",
+    row = 0,
+    col = 1,
+  },
+}
