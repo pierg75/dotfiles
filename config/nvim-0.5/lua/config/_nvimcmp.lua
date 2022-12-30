bedddffd0f92498c89ee9dd7e02ddea7f1cb4fec
Pierguido Lambri <pierg75@yahoo.it>
2022-12-30 15:11:57 +0000

    Fixed cmp
    Signed-off-by: Pierguido Lambri <pierg75@yahoo.it>

--- config/nvim-0.5/lua/config/_nvimcmp.lua
+++ config/nvim-0.5/lua/config/_nvimcmp.lua
@@ -3,12 +3,17 @@ local has_words_before = function()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-local status_ok, luasnip = pcall(require, "luasnip")
+local check_backspace = function()
+	local col = vim.fn.col(".") - 1
+	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+end
+
+local status_ok, cmp = pcall(require, "cmp")
 if not status_ok then
 	return
 end
 
-local status_ok, cmp = pcall(require, "cmp")
+local status_ok, luasnip = pcall(require, "luasnip")
 if not status_ok then
 	return
 end
@@ -18,10 +23,12 @@ if not status_ok then
 	return
 end
 
+require("luasnip/loaders/from_vscode").lazy_load()
+
 cmp.setup({
   snippet = {
     expand = function(args)
-      require("luasnip").lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
   window = {
@@ -78,6 +85,8 @@ cmp.setup({
         luasnip.expand_or_jump()
       elseif has_words_before() then
         cmp.complete()
+			elseif check_backspace() then
+				fallback()
       else
         fallback()
       end
@@ -96,10 +105,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    })
+    ['<CR>'] = cmp.mapping.confirm()
   },
   sources = cmp.config.sources({
       { name = "nvim_lsp" },
@@ -116,7 +122,18 @@ cmp.setup({
 			},
     }, {
       { name = 'buffer' },
-  })
+  }),
+	confirm_opts = {
+		behavior = cmp.ConfirmBehavior.Replace,
+		select = false,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	experimental = {
+		ghost_text = true,
+	}
 })
 
 require("cmp_dictionary").setup({
