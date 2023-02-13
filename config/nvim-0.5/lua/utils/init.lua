f83fbfe3d69897366388369a2e3b6c0f310d24db
Pierguido Lambri <plambri@redhat.com>
2023-02-13 21:33:50 +0000

    Reworked a bit the mapping function
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim-0.5/lua/utils/init.lua
+++ config/nvim-0.5/lua/utils/init.lua
@@ -8,9 +8,11 @@ function utils.opt(scope, key, value)
 end
 
 function utils.map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.keymap.set(mode, lhs, rhs, options)
 end
 
 function utils.file_exists(fname)
