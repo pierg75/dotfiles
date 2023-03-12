3c8fb13670e68fca52ec0450a335247b4f091824
Pierguido Lambri <plambri@redhat.com>
2023-03-12 14:47:55 +0000

    Added which-key plugin
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim-0.5/lua/plugins.lua
+++ config/nvim-0.5/lua/plugins.lua
@@ -255,6 +255,18 @@ return packer.startup(function(use)
     requires = { 'nvim-tree/nvim-web-devicons' }
   }
 
+  use { 'folke/which-key.nvim',
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
