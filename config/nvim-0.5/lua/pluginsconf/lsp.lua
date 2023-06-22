662bfb34ee07d3c21db411d6fc299cf61173c84e
Pier <pierg75@yahoo.it>
2023-06-22 15:32:40 +0100

    Removed a plugin (fidget)
    Signed-off-by: Pier <pierg75@yahoo.it>

--- config/nvim-0.5/lua/pluginsconf/lsp.lua
+++ config/nvim-0.5/lua/pluginsconf/lsp.lua
@@ -5,12 +5,6 @@ local M = {
     {
       "hrsh7th/cmp-nvim-lsp",
     },
-		-- Useful status updates for LSP
-		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-		{
-			"j-hui/fidget.nvim",
-			opts = {}
-		},
 		-- Additional lua configuration, makes nvim stuff amazing!
 		{
 			'folke/neodev.nvim',
