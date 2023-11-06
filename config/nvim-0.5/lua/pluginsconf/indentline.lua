0aa6bdaf3945a4c787f7acc9d35328d3e878f4f7
Pierguido Lambri <plambri@redhat.com>
2023-11-06 09:58:23 +0000

    Upgraded indentline to 3
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim-0.5/lua/pluginsconf/indentline.lua
+++ config/nvim-0.5/lua/pluginsconf/indentline.lua
@@ -1,20 +1,15 @@
 local M = {
   "lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
   event = "BufReadPre",
 }
 
-M.opts = {
-  char = "▏",
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = true,
-  use_treesitter = true,
-  show_current_context = true,
-  buftype_exclude = { "terminal", "nofile" },
-  filetype_exclude = {
-    "help",
-    "packer",
-    "NvimTree",
-  },
-}
+function M.config()
+	local status_ok, indent = pcall(require, "ibl")
+	if not status_ok then
+		return
+	end
+	indent.setup {}
+end
 
 return M
