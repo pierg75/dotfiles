f98b41cdc7e939e37f36126ff447e00b3d700c13
Pierguido Lambri <plambri@redhat.com>
2024-01-27 09:11:47 +0000

    Few treesitter tweaks
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim-0.5/lua/pluginsconf/treesitter.lua
+++ config/nvim-0.5/lua/pluginsconf/treesitter.lua
@@ -1,45 +1,40 @@
 local M = {
-  "nvim-treesitter/nvim-treesitter",
-  event = "BufReadPost",
-  dependencies = {
-    {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-      event = "VeryLazy",
-    },
-    {
-      "nvim-tree/nvim-web-devicons",
-      event = "VeryLazy",
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = "BufReadPost",
+	dependencies = {
+		{
+			"nvim-tree/nvim-web-devicons",
+			event = "VeryLazy",
+		},
+	},
 }
 function M.config()
-  local treesitter = require "nvim-treesitter"
-  local configs = require "nvim-treesitter.configs"
+	local treesitter = require "nvim-treesitter"
+	local configs = require "nvim-treesitter.configs"
 
-  configs.setup {
-    ensure_installed = { "lua", "markdown", "markdown_inline", "bash",
-												 "python", "rust", "toml", "c", "cpp", "go",
-												 "vim"}, -- put the language you want in this array
-    -- ensure_installed = "all", -- one of "all" or a list of languages
-    ignore_install = { "" },                                                       -- List of parsers to ignore installing
-    sync_install = false,                                                          -- install languages synchronously (only applied to `ensure_installed`)
+	configs.setup {
+		ensure_installed = { "lua", "luadoc", "markdown", "markdown_inline", "bash",
+			"python", "rust", "toml", "c", "cpp", "go", "vim", "diff", "regex", "yaml" }, -- put the language you want in this array
+		ignore_install = { "" },                                                     -- List of parsers to ignore installing
+		sync_install = false,                                                        -- install languages synchronously (only applied to `ensure_installed`)
+		-- Automatically install missing parsers when entering buffer
+		-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+		auto_install = true,
 
-    highlight = {
-      enable = true,       -- false will disable the whole extension
+		highlight = {
+			enable = true,    -- false will disable the whole extension
 			additional_vim_regex_highlighting = false,
-      disable = { "css" }, -- list of language that will be disabled
-    },
-    autopairs = {
-      enable = true,
-    },
-    indent = { enable = true, disable = { "css" } },
-
-		rainbow = {
-			enable = true,
-			extended_mode = true,
-			max_file_lines = nil,
+			disable = { "css" }, -- list of language that will be disabled
 		},
-  }
+		autopairs = {
+			enable = true,
+		},
+		indent = {
+			enable = true,
+			disable = { "css" }
+		},
+	}
 end
 
 return M
