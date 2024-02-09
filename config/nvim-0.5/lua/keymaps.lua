ece6f87afec06c954e9bfd207ec3a849efe398a7
Pier <pierg75@yahoo.it>
2024-02-09 08:21:20 +0000

    Added lsp references
    Signed-off-by: Pier <pierg75@yahoo.it>

--- config/nvim-0.5/lua/keymaps.lua
+++ config/nvim-0.5/lua/keymaps.lua
@@ -57,13 +57,13 @@ keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
 -- fzf-lua
 keymap("n", "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>", opts)
+keymap("n", "<leader>fr", "<cmd>lua require('fzf-lua').resume()<CR>", opts)
 keymap("n", "<leader>fl", "<cmd>lua require('fzf-lua').live_grep({ multiprocess=true, debug=true })<CR>", opts)
 keymap("n", "<leader>ft", "<cmd>lua require('fzf-lua').grep({ multiprocess=true, debug=true })<CR>", opts)
 keymap("n", "<leader>fT", "<cmd>lua require('fzf-lua').tags()<CR>", opts)
 keymap("n", "<leader>fb", "<cmd>lua require('fzf-lua').buffer<CR>", opts)
-keymap("n", "<leader>fb", "<cmd>lua require('fzf-lua').buffer<CR>", opts)
-keymap("n", "<leader>fr", "<cmd>lua require('fzf-lua').lsp_references<CR>", opts)
-keymap("n", "<leader>fd", "<cmd>lua require('fzf-lua').lsp_definitions<CR>", opts)
+keymap("n", "<leader>flr", "<cmd>lua require('fzf-lua').lsp_references<CR>", opts)
+keymap("n", "<leader>fld", "<cmd>lua require('fzf-lua').lsp_definitions<CR>", opts)
 
 -- Git
 keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
