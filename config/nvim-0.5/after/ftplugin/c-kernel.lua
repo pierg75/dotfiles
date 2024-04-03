e8c21e7a8c9e0945e12148ccb2f2c1e0f2c04a1a
Pierguido Lambri <plambri@redhat.com>
2024-04-03 21:11:11 +0100

    Moved the ftplugins into 'after'
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim-0.5/after/ftplugin/c-kernel.lua
+++ config/nvim-0.5/after/ftplugin/c-kernel.lua
@@ -4,4 +4,4 @@ vim.opt_local.textwidth = 78
 
 vim.opt_local.autoindent = smartindent
 vim.opt_local.smarttab = true
-vim.opt_local.backspace = eol,start,indent
+vim.opt_local.backspace = eol, start, indent
---
+++ config/nvim-0.5/after/ftplugin/c.lua
@@ -0,0 +1,9 @@
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.textwidth = 78
+
+vim.opt_local.autoindent = smartindent
+vim.opt_local.smarttab = true
+vim.opt_local.backspace = eol, start, indent
+
+vim.opt_local.cindent = true
