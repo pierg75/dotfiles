bbb5d6726bbfe0f7af32948a07072b0574cef898
Pierguido Lambri <plambri@redhat.com>
2024-04-05 09:47:17 +0100

    Update c-kernel filetype to use TS c
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim-0.5/after/ftplugin/c-kernel.lua
+++ config/nvim-0.5/after/ftplugin/c-kernel.lua
@@ -5,3 +5,5 @@ vim.opt_local.textwidth = 78
 vim.opt_local.autoindent = smartindent
 vim.opt_local.smarttab = true
 vim.opt_local.backspace = eol, start, indent
+
+vim.treesitter.language.register('c', { 'c-kernel' })
