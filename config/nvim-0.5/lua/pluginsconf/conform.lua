1928a1811711f2d95d3ac813c0c672fb05061783
Pierguido Lambri <plambri@redhat.com>
2024-04-11 11:16:27 +0100

    Added c formatter
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim-0.5/lua/pluginsconf/conform.lua
+++ config/nvim-0.5/lua/pluginsconf/conform.lua
@@ -1,19 +1,20 @@
 local m = {
-  "stevearc/conform.nvim",
+	"stevearc/conform.nvim",
 }
 
 function m.config()
-  require("conform").setup({
-    formatters_by_ft = {
-      python = { "black" },
-      markdown = { "inject" },
-      ["*"] = { "trim_whitespace", "trim_newlines" },
-    },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-  })
+	require("conform").setup({
+		formatters_by_ft = {
+			python = { "black" },
+			markdown = { "inject" },
+			c = { "clang-format" },
+			["*"] = { "trim_whitespace", "trim_newlines" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	})
 end
 
 return m
