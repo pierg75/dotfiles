a32b4a7a5be760a056311d94b67b5ad266843149
Pierguido Lambri <plambri@redhat.com>
2024-04-13 10:10:45 +0100

    Reworked a bit lazy.nvim initialization
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim-0.5/lua/plugins.lua
+++ config/nvim-0.5/lua/plugins.lua
@@ -1,14 +1,37 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("pluginsconf")
+require("lazy").setup({
+	spec = {
+		{ import = "pluginsconf" },
+	},
+	checker = {
+		enabled = false, -- automatically check for plugin updates
+		notify = true
+	},
+	install = {
+		missing = true,
+		colorscheme = { "tokyonight" }
+	},
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip",
+				"tarPlugin",
+				"tohtml",
+				"zipPlugin",
+			},
+		},
+	},
+})
