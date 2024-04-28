dccc3b5a0f32bc165e11b7f8340361b22f872832
Pierguido Lambri <plambri@redhat.com>
2024-04-28 21:43:51 +0100

    Adjusted treesitter config
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim-0.5/lua/pluginsconf/treesitter.lua
+++ config/nvim-0.5/lua/pluginsconf/treesitter.lua
@@ -5,6 +5,7 @@ local M = {
 	dependencies = {
 		{
 			"nvim-tree/nvim-web-devicons",
+			"nvim-treesitter/nvim-treesitter-textobjects",
 			event = "VeryLazy",
 		},
 	},
@@ -15,15 +16,13 @@ function M.config()
 	configs.setup {
 		ensure_installed = { "lua", "luadoc", "markdown", "markdown_inline", "bash",
 			"python", "rust", "toml", "c", "cpp", "go", "vim", "diff", "regex", "yaml",
-			"vimdoc" },          -- put the language you want in this array
-		ignore_install = { "" }, -- List of parsers to ignore installing
-		sync_install = false,  -- install languages synchronously (only applied to `ensure_installed`)
-		-- Automatically install missing parsers when entering buffer
-		-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+			"vimdoc", "query" },
+		ignore_install = { "" },
+		sync_install = false,
 		auto_install = true,
 
 		highlight = {
-			enable = true, -- false will disable the whole extension
+			enable = true,
 			additional_vim_regex_highlighting = false,
 			disable = function(lang, buf)
 				local max_filesize = 100 * 1024 -- 100 KB
@@ -39,6 +38,54 @@ function M.config()
 		indent = {
 			enable = true,
 			disable = { "css" }
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<Leader>ss",
+				node_incremental = "<Leader>si",
+				scope_incremental = "<Leader>sc",
+				node_decremental = "<Leader>sd",
+			},
+		},
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					-- You can use the capture groups defined in textobjects.scm
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					-- You can optionally set descriptions to the mappings (used in the desc parameter of
+					-- nvim_buf_set_keymap) which plugins like which-key display
+					["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+					-- You can also use captures from other query groups like `locals.scm`
+					["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+				},
+				-- You can choose the select mode (default is charwise 'v')
+				--
+				-- Can also be a function which gets passed a table with the keys
+				-- * query_string: eg '@function.inner'
+				-- * method: eg 'v' or 'o'
+				-- and should return the mode ('v', 'V', or '<c-v>') or a table
+				-- mapping query_strings to modes.
+				selection_modes = {
+					['@parameter.outer'] = 'v', -- charwise
+					['@function.outer'] = 'V', -- linewise
+					['@class.outer'] = '<c-v>', -- blockwise
+				},
+				-- If you set this to `true` (default is `false`) then any textobject is
+				-- extended to include preceding or succeeding whitespace. Succeeding
+				-- whitespace has priority in order to act similarly to eg the built-in
+				-- `ap`.
+				--
+				-- Can also be a function which gets passed a table with the keys
+				-- * query_string: eg '@function.inner'
+				-- * selection_mode: eg 'v'
+				-- and should return true or false
+				include_surrounding_whitespace = true,
+			},
 		},
 	}
 end
