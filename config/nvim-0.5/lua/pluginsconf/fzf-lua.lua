2fbdc4b1d76023b3243e8741f34e8ba5afaa2f4b
Pier <pierg75@yahoo.it>
2024-05-14 15:19:02 +0100

    Added some keymaps to fzf-lua
    Signed-off-by: Pier <pierg75@yahoo.it>

--- config/nvim-0.5/lua/pluginsconf/fzf-lua.lua
+++ config/nvim-0.5/lua/pluginsconf/fzf-lua.lua
@@ -1,27 +1,57 @@
 local M = {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- calling `setup` is optional for customization
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- calling `setup` is optional for customization
 }
 
+
 function M.config()
-  require("fzf-lua").setup({
-    'skim',
-    winopts = {
-      preview = { default = 'bat_native' }
-    },
-    grep = {
-      rg_opts = "--sort-files --hidden --column --line-number --no-heading " ..
-          "--color=always --smart-case -g '!{.git,node_modules}/*'",
-      git_icons = false,
-      file_icons = false,
-    },
-    live_grep = {
-      git_icons = false,
-      file_icons = false,
-    },
-  })
+	local actions = require 'fzf-lua.actions'
+	require("fzf-lua").setup({
+		'skim',
+		winopts = {
+			preview = { default = 'bat_native' }
+		},
+		grep = {
+			rg_opts = "--sort-files --hidden --column --line-number --no-heading " ..
+					"--color=always --smart-case -g '!{.git,node_modules}/*'",
+			git_icons = false,
+			file_icons = false,
+		},
+		live_grep = {
+			git_icons = false,
+			file_icons = false,
+		},
+		global_resume = true,
+		keymap = {
+			builtin = {
+				["ctrl-u"] = "half-page-up",
+				["ctrl-d"] = "half-page-down",
+				["<C-f>"] = "preview-page-down",
+				["<C-b>"] = "preview-page-up",
+				["<C-a>"] = "toggle-all",
+			},
+			fzf = {
+				["ctrl-u"] = "half-page-up",
+				["ctrl-d"] = "half-page-down",
+				["ctrl-f"] = "preview-page-down",
+				["ctrl-b"] = "preview-page-up",
+				["ctrl-a"] = "toggle-all",
+			},
+		},
+		actions = {
+			files = {
+				["ctrl-q"] = actions.file_sel_to_qf,
+			},
+		},
+		blines = {
+			fzf_opts = {
+				['--no-multi'] = false,
+				['--multi'] = true,
+			}
+		},
+	})
 end
 
 return M
