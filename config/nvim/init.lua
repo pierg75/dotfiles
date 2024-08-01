50d26a058214e27e7f08a7b5d205761994f402b8
Pierguido Lambri <plambri@redhat.com>
2024-08-01 21:45:03 +0100

    Added function to autoload user scripts
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -11,3 +11,7 @@ require "keymaps"
 require "ft"
 
 vim.cmd 'colorscheme tokyonight'
+
+for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath "config" .. "/lua/user_functions", [[v:val =~ '\.lua$']])) do
+	require("user_functions." .. file:gsub("%.lua$", ""))
+end
