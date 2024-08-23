1c8b3da4963ec21adc4a4453e9ae1cd2c63d160c
Pierguido Lambri <plambri@redhat.com>
2024-08-23 09:22:08 +0100

    Disable Black, too many changes
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim/lua/pluginsconf/conform.lua
+++ config/nvim/lua/pluginsconf/conform.lua
@@ -5,17 +5,23 @@ local m = {
 function m.config()
 	require("conform").setup({
 		formatters_by_ft = {
-			python = { "black" },
+			-- python = { "isort", "black" },
 			markdown = { "inject" },
 			c = { "clang-format" },
 			sh = { "beautysh" },
 			lua = { "lua_format" },
+			rust = { "rustfmt", lsp_format = "fallback" },
 			["*"] = { "trim_whitespace", "trim_newlines" },
 		},
 		format_on_save = {
-			timeout_ms = 500,
+			timeout_ms = 3000,
 			lsp_fallback = true,
 		},
+		--formatters = {
+		--	black = {
+		--		prepend_args = { '--fast' },
+		--	},
+		--},
 	})
 end
 
