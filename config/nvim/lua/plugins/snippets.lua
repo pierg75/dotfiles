9cb042e6c59c2edf4e1e32264bacc7d66511e7d5
Pierguido Lambri <plambri@redhat.com>
2024-09-12 13:59:23 +0100

    Added snippets
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

---
+++ config/nvim/lua/plugins/snippets.lua
@@ -0,0 +1,11 @@
+local M = {
+    {
+        'L3MON4D3/LuaSnip',
+        conifg = function(opts)
+            require('luasnip').setup(opts)
+            require('luasnip.loaders.from_snipmate').load({ paths = {"./snippets"} })
+        end,
+    },
+}
+
+return M
