ea733b42a02b9b7addd4097c70f9bcf01daf6281
Pierguido Lambri <plambri@redhat.com>
2024-12-19 21:59:54 +0000

    Clean up formatting
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -1,39 +1,35 @@
 return {
-  'saghen/blink.cmp',
-  lazy = false,
-  dependencies = {	'rafamadriz/friendly-snippets',
-										'L3MON4D3/LuaSnip', version = 'v2.*' },
+	'saghen/blink.cmp',
+	lazy = false,
+	dependencies = {
+		'rafamadriz/friendly-snippets',
+		'L3MON4D3/LuaSnip',
+		version = 'v2.*'
+	},
 
-  version = 'v0.*',
-  opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- see the "default configuration" section below for full documentation on how to define
-    -- your own keymap.
-    keymap = { preset = 'default' },
+	version = 'v0.*',
+	opts = {
+		keymap = { preset = 'default' },
 
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono'
-    },
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = 'mono'
+		},
 
 		snippets = {
-      expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require('luasnip').jumpable(filter.direction)
-        end
-        return require('luasnip').in_snippet()
-      end,
-      jump = function(direction) require('luasnip').jump(direction) end,
-    },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
+			expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+			active = function(filter)
+				if filter and filter.direction then
+					return require('luasnip').jumpable(filter.direction)
+				end
+				return require('luasnip').in_snippet()
+			end,
+			jump = function(direction) require('luasnip').jump(direction) end,
+		},
+		sources = {
+			default = { 'lsp', 'path', 'snippets', 'buffer' },
+		},
 
-    -- experimental signature help support
-    -- signature = { enabled = true }
 		completion = {
 			accept = {
 				auto_brackets = {
@@ -42,11 +38,7 @@ return {
 			}
 		},
 
-		signature = {enabled = true}
+		signature = { enabled = true }
 
-  },
-  -- allows extending the providers array elsewhere in your config
-  -- without having to redefine it
-  opts_extend = { "sources.default" }
+	},
 }
-
