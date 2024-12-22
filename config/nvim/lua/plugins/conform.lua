49726d6d1c8846fc17b0f853b497bfc0879586d8
Pierguido Lambri <plambri@redhat.com>
2024-12-22 10:10:57 +0000

    Removed unavailable formatters
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -6,10 +6,8 @@ function m.config()
 	require("conform").setup({
 		formatters_by_ft = {
 			-- python = { "isort", "black" },
-			markdown = { "inject" },
 			c = { "clang-format" },
 			sh = { "beautysh" },
-			lua = { "lua-format" },
 			rust = { "rustfmt", lsp_format = "fallback" },
 			["*"] = { "trim_whitespace", "trim_newlines" },
 		},
