c5b54472664aba7c75215836e078cebaff3c20a4
Pierguido Lambri <plambri@redhat.com>
2024-12-22 10:11:39 +0000

    Fixed lazydev for the newest blink
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim/lua/plugins/lazydev-nvim.lua
+++ config/nvim/lua/plugins/lazydev-nvim.lua
@@ -1,29 +1,30 @@
 return {
-  "folke/lazydev.nvim",
-  ft = "lua",
-  cmd = "LazyDev",
-  opts = {
-    library = {
-      { path = "luvit-meta/library", words = { "vim%.uv" } },
-      { path = "LazyVim", words = { "LazyVim" } },
-      { path = "lazy.nvim", words = { "LazyVim" } },
-    },
-  },
-  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-	{ -- optional blink completion source for require statements and module annotations
-    "saghen/blink.cmp",
-    opts = {
-      sources = {
-        -- add lazydev to your completion providers
-        completion = {
-          enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
-        },
-        providers = {
-          -- dont show LuaLS require statements when lazydev has items
-          lsp = { fallback_for = { "lazydev" } },
-          lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
-        },
-      },
-    },
-  }
+	"folke/lazydev.nvim",
+	ft = "lua",
+	cmd = "LazyDev",
+	opts = {
+		library = {
+			{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			{ path = "LazyVim",            words = { "LazyVim" } },
+			{ path = "lazy.nvim",          words = { "LazyVim" } },
+		},
+	},
+	{ "Bilal2453/luvit-meta", lazy = true },
+	{
+		"saghen/blink.cmp",
+		opts = {
+			sources = {
+				-- add lazydev to your completion providers
+				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						-- make lazydev completions top priority (see `:h blink.cmp`)
+						score_offset = 100,
+					},
+				},
+			},
+		},
+	}
 }
