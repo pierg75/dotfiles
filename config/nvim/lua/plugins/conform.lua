69c7ea89029eafbae4e0600d9427dfcf486626f7
Pierguido Lambri <plambri@redhat.com>
2025-03-26 08:30:43 +0000

    Don't use black for python and add ruff
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,21 +1,22 @@
 return {
 	{
 		"stevearc/conform.nvim",
+		lazy = true,
 		opts = {},
 		config = function()
 			local conform = require("conform")
 			conform.setup({
 				formatters_by_ft = {
 					lua = { "stylua" },
-					python = { "isort", "black" },
+					python = { "isort", "ruff_format", "ruff_organize_imports", lsp_format = "fallback" },
 					rust = { "rustfmt", lsp_format = "fallback" },
 					c = { "clang-format" },
 					sh = { "beautysh" },
 					["*"] = { "trim_whitespace", "trim_newlines" },
 				},
 				formatters = {
-					black = {
-						prepend_args = { "--fast" },
+					ruff_organize_imports = {
+						append_args = { "--line-length", "110" },
 					},
 				},
 			})
