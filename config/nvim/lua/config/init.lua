3167e1f96fa5d27f6c5fca51461250266fc75f5a
Pierguido Lambri <plambri@redhat.com>
2026-01-28 08:20:09 +0000

    Added autocmd for treesitter
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim/lua/config/init.lua
+++ config/nvim/lua/config/init.lua
@@ -2,3 +2,12 @@ require("config.lazy")
 require("config.options")
 require("config.keymaps")
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '<filetype>' },
+  callback = function()
+		vim.treesitter.start()
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+		vim.wo[0][0].foldmethod = 'expr'
+	end,
+})
