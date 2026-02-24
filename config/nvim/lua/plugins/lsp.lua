2e678b17aa3480289c025dfe57e4ca14ba43c33d
Pierguido Lambri <plambri@redhat.com>
2026-02-24 20:37:09 +0000

    Updated lsp to the kickstart config
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -12,10 +12,8 @@ return {
 					},
 				},
 			},
-			{ "williamboman/mason-lspconfig.nvim" },
 			{ "saghen/blink.cmp" },
 			{ "williamboman/mason.nvim", config = true },
-			{ "williamboman/mason-lspconfig.nvim" },
 			{
 				"WhoIsSethDaniel/mason-tool-installer.nvim",
 				{
@@ -25,104 +23,142 @@ return {
 			},
 		},
 		config = function()
-			local mason_lspconfig = require("mason-lspconfig")
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			local capabilities = vim.tbl_deep_extend(
-				"force",
-				capabilities,
-				require("blink.cmp").get_lsp_capabilities({
-					textDocument = {
-						completion = {
-							completionItem = {
-								snippetSupport = true,
-							},
-						},
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+				callback = function(event)
+					local map = function(keys, func, desc, mode)
+						mode = mode or "n"
+						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+
+					-- Rename the variable under your cursor.
+					--  Most Language Servers support renaming across files, etc.
+					map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+					-- Execute a code action, usually your cursor needs to be on top of an error
+					-- or a suggestion from your LSP for this to activate.
+					map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+
+					-- WARN: This is not Goto Definition, this is Goto Declaration.
+					--  For example, in C this would take you to the header.
+					map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+					-- The following two autocommands are used to highlight references of the
+					-- word under your cursor when your cursor rests there for a little while.
+					--    See `:help CursorHold` for information about when this is executed
+					--
+					-- When you move your cursor, the highlights will be cleared (the second autocommand).
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if client and client:supports_method("textDocument/documentHighlight", event.buf) then
+						local highlight_augroup =
+							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							buffer = event.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.document_highlight,
+						})
+
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							buffer = event.buf,
+							group = highlight_augroup,
+							callback = vim.lsp.buf.clear_references,
+						})
+
+						vim.api.nvim_create_autocmd("LspDetach", {
+							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+							callback = function(event2)
+								vim.lsp.buf.clear_references()
+								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+							end,
+						})
+					end
+
+					-- The following code creates a keymap to toggle inlay hints in your
+					-- code, if the language server you are using supports them
+					--
+					-- This may be unwanted, since they displace some of your code
+					if client and client:supports_method("textDocument/inlayHint", event.buf) then
+						map("<leader>th", function()
+							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+						end, "[T]oggle Inlay [H]ints")
+					end
+				end,
+			})
+			-- LSP servers and clients are able to communicate to each other what features they support.
+			--  By default, Neovim doesn't support everything that is in the LSP specification.
+			--  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
+			--  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+			local servers = {
+				clangd = {},
+				gopls = {},
+				lua_ls = {},
+				basedpyright = {
+					analysis = {
+						typeCheckingMode = "basic",
+						ignore = { "*" },
 					},
-					workspace = {
-						didChangeWatchedFiles = {
-							relativePatternSupport = true,
-						},
-					},
-				})
-			)
-			capabilities = vim.tbl_deep_extend("force", capabilities, {
-				textDocument = {
-					foldingRange = {
-						dynamicRegistration = false,
-						lineFoldingOnly = true,
-					},
+					disableOrganizeImports = true,
 				},
+				ruff = {},
+			}
+
+			-- Ensure the servers and tools above are installed
+			--
+			-- To check the current status of installed tools and/or manually install
+			-- other tools, you can run
+			--    :Mason
+			--
+			-- You can press `g?` for help in this menu.
+			local ensure_installed = vim.tbl_keys(servers or {})
+			vim.list_extend(ensure_installed, {
+				"lua_ls",
+				"stylua",
+				"basedpyright",
+				"ruff",
+				"gopls",
 			})
 
-			local on_attach = function(client, bufnr)
-				vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
-				client.server_capabilities.document_formatting = true
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+			for name, server in pairs(servers) do
+				server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+				vim.lsp.config(name, server)
+				vim.lsp.enable(name)
 			end
 
-			mason_lspconfig.setup({
-				automatic_installation = false,
-				ensure_installed = { "lua_ls", "clangd", "gopls", "basedpyright", "ruff" },
-				handlers = {
-					function(server_name)
-						vim.lsp.enable(server_name())
-					end,
-				},
-			})
-
-			-- Python
-			vim.lsp.config("basedpyright", {
-				capabilities = capabilities,
-				on_attach = on_attach,
-				settings = {
-					basedpyright = {
-						analysis = {
-							typeCheckingMode = "basic",
-							ignore = { "*" },
-						},
-						disableOrganizeImports = true,
-						-- diagnosticMode = "openFilesOnly",
-						-- typeCheckingMode = "standard",
-						-- useLibraryCodeForTypes = true,
-					},
-				},
-			})
-
-			vim.lsp.config("ruff", {
-				capabilities = capabilities,
-				on_attach = on_attach,
-				settings = {
-					init_options = {
-						settings = {
-							logLevel = "error",
-						},
-					},
-				},
-			})
-
-			-- Lua
+			-- Special Lua Config, as recommended by neovim help docs
 			vim.lsp.config("lua_ls", {
-				capabilities = capabilities,
-				on_attach = on_attach,
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
+				on_init = function(client)
+					if client.workspace_folders then
+						local path = client.workspace_folders[1].name
+						if
+							path ~= vim.fn.stdpath("config")
+							and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+						then
+							return
+						end
+					end
+
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+						runtime = {
+							version = "LuaJIT",
+							path = { "lua/?.lua", "lua/?/init.lua" },
 						},
-					},
+						workspace = {
+							checkThirdParty = false,
+							-- NOTE: this is a lot slower and will cause issues when working on your own configuration.
+							--  See https://github.com/neovim/nvim-lspconfig/issues/3189
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+					})
+				end,
+				settings = {
+					Lua = {},
 				},
 			})
-
-			-- C
-			vim.lsp.config("clangd", {
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
-
-			-- Go
-			vim.lsp.config("gopls", {
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
+			vim.lsp.enable("lua_ls")
 		end,
 	},
 }
